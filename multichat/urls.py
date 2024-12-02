7fbdc457e6402e5655200cf6e674198fab397c17
Andile Mnukwa <andile.reeman.com>
2024-12-02 10:53:23 +0200

    Initial commit

---
+++ multichat/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
+from chat.views import index
+
+urlpatterns = [
+    path('', index),  # Home page or index
+    path('login/', LoginView.as_view(template_name='login.html'), name='login'),  # Updated login path
+    path('logout/', LogoutView.as_view(), name='logout'),
+    path('admin/', admin.site.urls),
+]
